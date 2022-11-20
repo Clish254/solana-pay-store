ea6ae3f56122f37136dc85e7cc697d6e0eb9374f
Clish254 <clishilla@gmail.com>
2022-11-20 15:06:42 +0300

    feat: allow users to buy wallpapers and add new products to the store

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,75 @@
-import React from "react";
-import HeadComponent from '../components/Head';
+import React, { useEffect, useState } from 'react'
+import { PublicKey } from '@solana/web3.js'
+
+import { useWallet } from '@solana/wallet-adapter-react'
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+
+import HeadComponent from '../components/Head'
+import Product from '../components/Product'
+import CreateProduct from '../components/CreateProduct'
 
 // Constants
-const TWITTER_HANDLE = "_buildspace";
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const TWITTER_HANDLE = '_buildspace'
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`
 
 const App = () => {
-  
-  
+  const { publicKey } = useWallet()
+  const isOwner = publicKey
+    ? publicKey.toString() === process.env.NEXT_PUBLIC_OWNER_PUBLIC_KEY
+    : false
+  const [creating, setCreating] = useState(false)
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    if (publicKey) {
+      fetch(`/api/fetchProducts`)
+        .then(response => response.json())
+        .then(data => {
+          setProducts(data)
+          console.log('Products', data)
+        })
+    }
+  }, [publicKey])
+
+  const renderNotConnectedContainer = () => (
+    <div>
+      <img
+        src="/buildspacebuildyourfuturewallpaper.jpeg"
+        alt="buildspace build your future"
+        style={{ width: '409.6px', height: '230.4px' }}
+      />
+      <div className="button-container">
+        <WalletMultiButton className="cta-button connect-wallet-button" />
+      </div>
+    </div>
+  )
+
+  const renderItemBuyContainer = () => (
+    <div className="products-container">
+      {products.map(product => (
+        <Product key={product.id} product={product} />
+      ))}
+    </div>
+  )
+
   return (
     <div className="App">
-      <HeadComponent/>
+      <HeadComponent />
       <div className="container">
         <header className="header-container">
-          <p className="header"> 😳 Buildspace Emoji Store 😈</p>
-          <p className="sub-text">The only emoji store that accepts sh*tcoins</p>
+          <p className="header"> Buildspace wallpaper Store 🎉</p>
+          <p className="sub-text">Buy sick buildpace wallpapers using your favourite sh*tcoins</p>
+
+          {isOwner && (
+            <button className="create-product-button" onClick={() => setCreating(!creating)}>
+              {creating ? 'Close' : 'Create Product'}
+            </button>
+          )}
         </header>
 
         <main>
-          <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
+          {creating && <CreateProduct />}
+          {publicKey ? renderItemBuyContainer() : renderNotConnectedContainer()}
         </main>
 
         <div className="footer-container">
@@ -27,12 +78,11 @@ const App = () => {
             className="footer-text"
             href={TWITTER_LINK}
             target="_blank"
-            rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+            rel="noreferrer">{`built on @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
